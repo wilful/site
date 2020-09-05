de4f1585e12a2c6cf1cc14e89e83117067cc5551
Andrey Semenov <a_semenov@gcore.lu>
2020-09-05 14:19:34 +0500

    Fix pelican config options

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'wilful'
 SITENAME = 'YODA'
-SITEURL = ''
+SITEURL = 'https://srv-nix.com'
 
 PATH = 'content'
 
@@ -20,14 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 70
 
