73877ee45843a946b5944ea4b6640db7b48833e5
Andrey Semenov <a_semenov@gcore.lu>
2021-06-21 16:09:59 +0300

    Test favicon settings

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -23,6 +23,16 @@ YANDEX_ANALYTICS = True
 #DISQUS_SITENAME = 'srv-nix-com'
 VK_SITENAME = 'Srv-Nix'
 GITHUB_URL = 'https://github.com/wilful/srv-nix.com'
+STATIC_PATHS = ['images']
+EXTRA_PATH_METADATA = {
+    'images/robots.txt': {'path': 'robots.txt'},
+    'images/favicon.ico': {'path': 'favicon.ico'},
+    'images/android-chrome-192x192.png': {'path': 'android-chrome-192x192.png'},
+    'images/android-chrome-512x512.png': {'path': 'android-chrome-512x512.png'},
+    'images/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'images/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'images/favicon-32x32.png': {'path': 'favicon-32x32.png'}
+}
 
 # Blogroll
 LINKS = (('Repository', 'https://bitbucket.org/wi1fu1/'),)
